9bb93406a9c467c31c078fc8cca7d9fc10937962
KiranA994 <kiranagrasala13@gmail.com>
2024-04-22 16:30:44 +0530

    url changed

--- src/services/baseUrl.js
+++ src/services/baseUrl.js
@@ -1,3 +1,6 @@
 // path of backend
 
-export const serverUrl = 'http://localhost:4000'
+// export const serverUrl = 'http://localhost:4000'
+
+export const serverUrl = 'https://media-player-backend-7vw0.onrender.com'
+
